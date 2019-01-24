49395c46ee61e28ea2d3d036522a80392c12e0e0
8gandalf <fladnag8@gmail.com>
2019-01-24 09:17:33 -0800

    clean

--- test-api-gw-method.py
+++ test-api-gw-method.py
@@ -10,7 +10,7 @@ import rsa
 import s3transfer        
 
 # api gateway testing
-url = 'https://i0hs6b0gn5.execute-api.us-west-2.amazonaws.com/test/cmd?param1='
+url = '<URL>'
 cmds = ['ls',
         'echo%20hello',
         'history',
@@ -19,9 +19,6 @@ cmds = ['ls',
         'rm%20test_file',
         'cat%20lambda_function.py'
         ]
-
-# Extract the endpoint resource name
-file_name = url.split("/")[-1].replace('.zip', '')        
 
 for cmd in cmds:
     test = url + cmd
